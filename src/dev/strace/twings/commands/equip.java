b39f5fba7320ec004ee95bccae5f4e8d2bc1f2e3
MrStrace <jason.holweg@strace.dev>
2021-06-08 01:12:59 +0200

    new message

--- src/dev/strace/twings/commands/equip.java
+++ src/dev/strace/twings/commands/equip.java
@@ -74,7 +74,7 @@ public class equip extends SubCommands {
 			 * This will be added in the config soon.
 			 * 
 			 */
-			p.sendMessage(Main.getInstance().getPrefix() + " §cParticle not found. All Particles: §d(/twings list)");
+			p.sendMessage(Main.getInstance().getMsg().getWingnotfound(args[1]));
 			break;
 		default:
 			break;
