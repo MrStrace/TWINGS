48f278cdc6fb7b74720c002b5f32461bb3dfe02a
MrStrace <jason.holweg@strace.dev>
2021-06-25 04:39:41 +0200

    fixed show message option and added comments

--- src/dev/strace/twings/listener/PlayerDeathListener.java
+++ src/dev/strace/twings/listener/PlayerDeathListener.java
@@ -9,17 +9,36 @@ import dev.strace.twings.Main;
 import dev.strace.twings.players.CurrentWings;
 import dev.strace.twings.utils.ConfigManager;
 
+/**
+ * 
+ * @author Jason Holweg [STRACE] <b>TWINGS</b><br>
+ *         Website: <a>https://strace.dev/</a><br>
+ *         GitHub: <a>https://github.com/MrStrace</a><br>
+ *         Created: Jun 24, 2021<br>
+ *
+ */
 public class PlayerDeathListener implements Listener {
 
+	// Player Death Event
 	@EventHandler
 	public void onDeath(PlayerDeathEvent e) {
 
+		// Checks if the entity is an player.
 		if (e.getEntity() instanceof Player) {
 			Player p = e.getEntity();
+
+			// creates the untildeath.yml file.
 			ConfigManager manager = new ConfigManager("untildeath");
+
+			/*
+			 * if the untildeath file contains the player it will remove the wings, since he
+			 * got wings equiped "untildeath". also he recieves a custom message.
+			 */
 			if (manager.getString(p.getUniqueId().toString()) != null) {
 				new CurrentWings().removeCurrentWing(p);
-				p.sendMessage(Main.getInstance().getMsg().getWingsgone());
+				// if show messages is enabled the player revieces the message.
+				if (Main.getInstance().getMsg().isShowMessages())
+					p.sendMessage(Main.getInstance().getMsg().getWingsgone());
 				manager.set(p.getUniqueId().toString(), null);
 				manager.save();
 			}
