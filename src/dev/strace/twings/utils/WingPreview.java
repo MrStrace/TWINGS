e0aa662e1961d2ca1883b9d4cf78b7e8bf188620
MrStrace <jason.holweg@strace.dev>
2021-06-25 04:46:16 +0200

    fixed bug

--- src/dev/strace/twings/utils/WingPreview.java
+++ src/dev/strace/twings/utils/WingPreview.java
@@ -8,10 +8,28 @@ import dev.strace.twings.Main;
 import dev.strace.twings.api.API;
 import dev.strace.twings.utils.objects.Wing;
 
+/**
+ * 
+ * @author Jason Holweg [STRACE] <b>TWINGS</b><br>
+ *         Website: <a>https://strace.dev/</a><br>
+ *         GitHub: <a>https://github.com/MrStrace</a><br>
+ *         Created: Jun 25, 2021<br>
+ *
+ */
 public class WingPreview {
+
 	private LocationBuilder builder;
+
 	public static Wing edit = null;
 
+	/**
+	 * this will run on reload/server start.
+	 * 
+	 * it will create a runnable which runs every few ticks (setup in config)
+	 * 
+	 * it will display a preview of set wings.
+	 * 
+	 */
 	public void enablePreview() {
 
 		new BukkitRunnable() {
@@ -19,7 +37,7 @@ public class WingPreview {
 
 			public void run() {
 				for (String locations : getLocs()) {
-					API api = new API(locations + ".yml");
+					API api = new API(builder.getString(locations + ".name") + ".yml");
 
 					if (edit != null) {
 						if (edit.getFile().getName().replace(".yml", "").equalsIgnoreCase(locations)) {
