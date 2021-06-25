c86af49da3b9cf0027741310e5fb3b1a3854e364
MrStrace <jason.holweg@strace.dev>
2021-06-25 04:39:13 +0200

    cleaning + comments

--- src/dev/strace/twings/listener/PlayerConnectionListener.java
+++ src/dev/strace/twings/listener/PlayerConnectionListener.java
@@ -1,23 +1,30 @@
 package dev.strace.twings.listener;
 
-import java.io.File;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import dev.strace.twings.Main;
 import dev.strace.twings.players.CurrentWings;
 import dev.strace.twings.utils.SendWings;
 
+/**
+ * 
+ * @author Jason Holweg [STRACE] <b>TWINGS</b><br>
+ *         Website: <a>https://strace.dev/</a><br>
+ *         GitHub: <a>https://github.com/MrStrace</a><br>
+ *         Created: Jun 24, 2021<br>
+ *
+ */
 public class PlayerConnectionListener implements Listener {
+
+	// Join Event
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
-		
+		// if the player has wings equiped on join he will get the animated timer.
 		if (CurrentWings.current.containsKey(p.getUniqueId())) {
 			SendWings.animated.put(p, 0);
 			SendWings.plus.put(p, true);
@@ -25,19 +32,17 @@ public class PlayerConnectionListener implements Listener {
 
 	}
 
+	// Disconnect Event
+
 	@EventHandler
 	public void onQuit(PlayerQuitEvent e) {
 		Player p = e.getPlayer();
+
+		// if the player disconnects he will get kicked out of any active maps.
 		if (CurrentWings.current.containsKey(p.getUniqueId())) {
 			SendWings.animated.remove(p);
 			SendWings.plus.remove(p);
 		}
 
 	}
-
-	public File getRandomWing() {
-		File[] file = new File(Main.getInstance().getDataFolder(), "wings").listFiles();
-		return file[0];
-	}
-	
 }
