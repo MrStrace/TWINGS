63dd1edd681b345725e04ad6fca52fde3596250c
MrStrace <jason.holweg@strace.dev>
2021-06-27 03:28:21 +0200

    deleted unnecessary runnable

--- src/dev/strace/twings/players/PlayWings.java
+++ src/dev/strace/twings/players/PlayWings.java
@@ -1,6 +1,7 @@
 package dev.strace.twings.players;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -10,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import dev.strace.twings.Main;
-import dev.strace.twings.utils.SendWings;
 import dev.strace.twings.utils.WingUtils;
 
 public class PlayWings {
@@ -31,42 +31,25 @@ public class PlayWings {
 								fil = f;
 							}
 						}
-						new SendWings().drawWings(p, WingUtils.winglist.get(fil));
+					WingUtils.winglist.get(fil).drawWings(p);
 					}
 					return;
 				}
-				HashMap<UUID, File> map = CurrentWings.getCurrent();
+				HashMap<UUID, ArrayList<File>> map = CurrentWings.getCurrent();
 				if (map.isEmpty())
 					return;
 				for (UUID uuid : map.keySet()) {
 					if (Bukkit.getPlayer(uuid) != null) {
 						Player p = Bukkit.getPlayer(uuid);
 						if (p.isOnline()) {
-							new SendWings().drawWings(p, WingUtils.winglist.get(map.get(uuid)));
+							for(File file : map.get(uuid))
+							WingUtils.winglist.get(file).drawWings(p);
 						}
 					}
-
 				}
 			}
 		}.runTaskTimerAsynchronously(Main.getInstance(), 0, Main.getInstance().config.getInt("UpdateRateTicks"));
 
 	}
-
-	public void enableAnimated() {
-        new BukkitRunnable() {
-            public void run() {
-                for (Player p : SendWings.animated.keySet()) {
-                    if (SendWings.animated.get(p) <= 30 && SendWings.plus.get(p))
-                    	SendWings.animated.put(p, SendWings.animated.get(p) + 2);
-                    else
-                    	SendWings.plus.put(p, false);
-                    if (SendWings.animated.get(p) > 0 && !SendWings.plus.get(p))
-                    	SendWings.animated.put(p, SendWings.animated.get(p) - 2);
-                    else
-                    	SendWings.plus.put(p, true);
-                }
-            }
-        }.runTaskTimerAsynchronously(Main.getInstance(), 20, 4);
-    }
 	
 }
