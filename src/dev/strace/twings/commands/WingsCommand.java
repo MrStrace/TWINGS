5d70dd2d23d42fa7b9b507544519416be6a9f928
#Strace <jason.holweg@strace.dev>
2021-07-04 03:57:09 +0200

    added Category support

--- src/dev/strace/twings/commands/WingsCommand.java
+++ src/dev/strace/twings/commands/WingsCommand.java
@@ -1,7 +1,9 @@
 package dev.strace.twings.commands;
 
 import dev.strace.twings.Main;
+import dev.strace.twings.utils.ConfigManager;
 import dev.strace.twings.utils.WingUtils;
+import dev.strace.twings.utils.gui.CategoryGUI;
 import dev.strace.twings.utils.gui.WingGUI;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -12,71 +14,73 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 
 /**
- * 
  * @author Jason Holweg [STRACE] <b>TWINGS</b><br>
- *         Website: <a>https://strace.dev/</a><br>
- *         GitHub: <a>https://github.com/MrStrace</a><br>
- *         Created: May 30, 2021<br>
- *
+ * Website: <a>https://strace.dev/</a><br>
+ * GitHub: <a>https://github.com/MrStrace</a><br>
+ * Created: May 30, 2021<br>
  */
 
 public class WingsCommand implements CommandExecutor {
 
-	// Liste aller SubCommands von Setup.
-	private final ArrayList<SubCommands> subcommands = new ArrayList<SubCommands>();
+    // Liste aller SubCommands von Setup.
+    private final ArrayList<SubCommands> subcommands = new ArrayList<SubCommands>();
 
-	/**
-	 * Initialisierung der SubCommands. Der ArrayListe werden die SubCommands
-	 * hinzugefugt und beim Start des Plugins initialisiert.
-	 */
-	public WingsCommand() {
-		subcommands.add(new Preview());
-		subcommands.add(new Edit());
-		subcommands.add(new UnEquip());
-		subcommands.add(new Equip());
-		subcommands.add(new List());
-		subcommands.add(new Reload());
-		subcommands.add(new Give());
-		subcommands.add(new Create());
-		subcommands.add(new Add());
-	}
+    /**
+     * Initialisierung der SubCommands. Der ArrayListe werden die SubCommands
+     * hinzugefugt und beim Start des Plugins initialisiert.
+     */
+    public WingsCommand() {
+        subcommands.add(new Preview());
+        subcommands.add(new Edit());
+        subcommands.add(new UnEquip());
+        subcommands.add(new Equip());
+        subcommands.add(new List());
+        subcommands.add(new Reload());
+        subcommands.add(new Give());
+        subcommands.add(new Create());
+        subcommands.add(new Add());
+    }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender instanceof Player) {
-			Player p = (Player) sender;
-			// if no arguments where given it will open the GUI.
-			if (args.length == 0) {
-				new WingGUI(WingUtils.winglist.size()).openGUI(p);
-				return true;
-			}
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            // if no arguments where given it will open the GUI.
+            if (args.length == 0) {
+                ConfigManager manager = new ConfigManager("CategoryGUI");
+                if (manager.getBoolean("enabled"))
+                    new CategoryGUI(manager.getInt("rows")).openGUI(p);
+                 else
+                    new WingGUI(WingUtils.winglist.size()).openGUI(p, "XXX");
+                return true;
+            }
 
-			handleMainCommand(args, p);
+            handleMainCommand(args, p);
 
-		}
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * this function will run all commands and subcommands.
-	 * 
-	 * @param args subcommands
-	 * @param p    player
-	 */
-	public void handleMainCommand(String[] args, Player p) {
+    /**
+     * this function will run all commands and subcommands.
+     *
+     * @param args subcommands
+     * @param p    player
+     */
+    public void handleMainCommand(String[] args, Player p) {
 
-		// for each subcommand it will check if the second argument equals the
-		// subcommand name if so it will perform the command.
-		for (SubCommands subcommand : subcommands) {
-			if (args[0].equalsIgnoreCase(subcommand.getName())) {
-				subcommand.perform(p, args);
-				return;
-			}
-		}
-		// if no command was found it will send the player this message.
-		p.sendMessage(Main.getInstance().getMsg().getNosuchcommand());
-		p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_BURP, 0.4F, 3);
-	}
+        // for each subcommand it will check if the second argument equals the
+        // subcommand name if so it will perform the command.
+        for (SubCommands subcommand : subcommands) {
+            if (args[0].equalsIgnoreCase(subcommand.getName())) {
+                subcommand.perform(p, args);
+                return;
+            }
+        }
+        // if no command was found it will send the player this message.
+        p.sendMessage(Main.getInstance().getMsg().getNosuchcommand());
+        p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_BURP, 0.4F, 3);
+    }
 
 }
